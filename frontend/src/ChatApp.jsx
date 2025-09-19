b378b5b83465be6636508aecb540dd88334c9e42
Donsir Arcilla <dokuseii747@gmail.com>
2025-09-19 14:54:36 +0800

    Update ChatApp.jsx

--- frontend/src/ChatApp.jsx
+++ frontend/src/ChatApp.jsx
@@ -210,7 +210,7 @@ export default function App() {
       boxSizing: "border-box",
     },
     chatWindow: {
-      height: "56vh",
+      height: "60vh",
       overflowY: "auto",
       background: "#fff",
       border: "3px solid #000",
